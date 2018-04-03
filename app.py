c182d03b40bd1c9479f6f59b841f6a3321a06b11
lifan <williamlifan@163.com>
2018-04-03 15:38:10 +0800

    增加了注册时重复用户名的检查，增加了注册时出错的页面提示。

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+# -*- coding: utf-8 -*-
+
 import sqlite3
-from flask import Flask, render_template, request, redirect, url_for, g
+from flask import Flask, render_template, request, redirect, url_for, g, flash, get_flashed_messages
 from model import User
 
 app = Flask(__name__)
 app.config["DATABASE"] = 'database.db'
+app.config["SECRET_KEY"] = '123qweasdzxc'
 
 
 def connect_db():
@@ -32,8 +35,9 @@ def init_db():
 
 
 def insert_user_to_db(user):
-	sql_insert = "INSERT INTO users (name, pwd, email, age, birthday, face) VALUES (?, ?, ?, ?, ?, ?)"
-	args = [user.name, user.pwd, user.email, user.age, user.birthday, user.face]
+	attrsRepr, placeHolders = user.getAttrsRepr()
+	sql_insert = "INSERT INTO users " + attrsRepr + " VALUES " + placeHolders
+	args = user.toList()
 	g.db.execute(sql_insert, args)
 	g.db.commit()
 
@@ -44,8 +48,55 @@ def query_users_from_db():
 	args = []
 	cur = g.db.execute(sql_query, args)
 	for item in cur.fetchall():
-		users.append(item)
+		user = User()
+		user.fromList(item[1:])
+		users.append(user)
 	return users
+
+
+def query_user_by_name(user_name):
+	sql_select = "select * from users where name=?"
+	args = [user_name]
+	cur = g.db.execute(sql_select, args)
+	item = cur.fetchone()
+	if item:
+		user = User()
+		user.fromList(item[1:])
+		return user
+	return None
+
+
+def delete_user_by_name(user_name):
+	sql_delete = "delete from users where name=?"
+	args = [user_name]
+	g.db.execute(sql_delete, args)
+	g.db.commit()
+
+
+def update_user_by_name(user_name, attrs):
+	if 'name' in attrs:
+		raise Exception(u'不能修改用户名')
+	setter = ''
+	cnt = len(attrs.keys())
+	i = 0
+	for key, value in attrs.iteritems():
+		setter += (key + '=?')
+		if i < cnt - 1:
+			setter += ', '
+		i += 1
+	sql_update = "update users set " + setter + " where name=?"
+	args = []
+	for key, value in attrs.iteritems():
+		args.append(value)
+	args.append(user_name)
+	g.db.execute(sql_update, args)
+	g.db.commit()
+
+
+def show_all_rows():
+	users = query_users_from_db()
+	for user in users:
+		print user.toList()
 
 
 @app.route('/')
@@ -55,8 +106,7 @@ def index():
 
 @app.route('/login/', methods=['GET', 'POST'])
 def user_login():
-	users = query_users_from_db()
-	print users
+	show_all_rows()
 	return render_template('user_login.html')
 
 
@@ -70,7 +120,12 @@ def user_regist():
 		user.age = request.form['user_age']
 		user.birthday = request.form['user_birthday']
 		user.face = request.form['user_face']
+		user_x = query_user_by_name(user.name)
+		if user_x:
+			flash(u"用户名已经存在！", category='error')
+			return render_template('user_regist.html')
 		insert_user_to_db(user)
+		flash(u"注册成功", category='ok')
 		return redirect(url_for("user_login", username=user.name))
 	return render_template('user_regist.html')
 
--- model.py
+++ model.py
@@ -9,3 +9,31 @@ class User(object):
 		self.age = age
 		self.birthday = birthday
 		self.face = face
+
+	def getAttrsRepr(self):
+		keys = self.__dict__.keys()
+		res = "("
+		ph = "("
+		for i, key in enumerate(keys):
+			res += key
+			ph += '?'
+			if i < len(keys) - 1:
+				res += ', '
+				ph += ', '
+		res += ')'
+		ph += ')'
+		return res, ph
+
+	def getAttrCnt(self):
+		return len(self.__dict__.keys())
+
+	def toList(self):
+		return [self.name, self.pwd, self.email, self.age, self.birthday, self.face]
+
+	def fromList(self, user_info):
+		self.name = user_info[0]
+		self.pwd = user_info[1]
+		self.email = user_info[2]
+		self.age = user_info[3]
+		self.birthday = user_info[4]
+		self.face = user_info[5]
